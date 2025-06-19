61c7a35b36dabbd6c16cca68e69f0a1c89256c77
remflood <m.aliraza.uol@gmail.com>
2025-06-19 10:54:58 +0500

    feat: add contact page

---
+++ src/app/contact-us/page.tsx
@@ -0,0 +1,42 @@
+import ContactForm from "@/components/ui/contact-form";
+import { Mail, MapPin, Phone } from "lucide-react";
+
+export default function ContactUs() {
+    return (
+        <main className="w-full flex flex-col items-center">
+            <section className="w-full flex flex-col gap-20 px-4 md:px-8 lg:px-38 2xl:px-56 py-16 lg:py-32">
+                <div className="flex flex-col items-center gap-8">
+                    <h1 className="w-full text-4xl md:text-6xl md:text-center font-inter-tight font-semibold">Let's Get In Touch</h1>
+                    <p className="md:text-2xl md:text-center font-albert-sans max-w-[1200px]">We would love to hear from you! Whether you have questions about a tour, want to arrange a private group, or need special accommodations (dietary or otherwise), our team is here to assist. Insha’Allah, we’ll ensure your travel plans are smooth and tailored to your needs.</p>
+                </div>
+                <div className="flex flex-col gap-12 md:gap-0 md:flex-row justify-between font-albert-sans text-lg md:text-2xl">
+                    <div className="w-full flex flex-col gap-4">
+                        <div className="border border-slate-300 rounded-2xl flex flex-row items-center justify-center size-16">
+                            <Mail size='60%' strokeWidth='1px' />
+                        </div>
+                        <div className="font-semibold">Email:</div>
+                        <div>info@arrahah.com</div>
+                    </div>
+                    <div className="w-full flex flex-col gap-4">
+                        <div className="border border-slate-300 rounded-2xl flex flex-row items-center justify-center size-16">
+                            <Phone size='60%' strokeWidth='1px' />
+                        </div>
+                        <div className="font-semibold">Phone:</div>
+                        <div>+1 (800) 123-ARAH (2724)</div>
+                    </div>
+                    <div className="w-full flex flex-col gap-4">
+                        <div className="border border-slate-300 rounded-2xl flex flex-row items-center justify-center size-16">
+                            <MapPin size='60%' strokeWidth='1px' />
+                        </div>
+                        <div className="font-semibold">Office:</div>
+                        <div>Lorem ipsum dolor sit amet consectetur. Turpis eu accumsan</div>
+                    </div>
+                </div>
+            </section>
+            <section className="w-full flex flex-col gap-15 lg:gap-20 px-4 md:px-8 lg:px-38 2xl:px-56 py-16 lg:py-32">
+                <h2 className="font-inter-tight text-[32px] lg:text-5xl font-semibold">Or fill this form below to get inquired!</h2>
+                <ContactForm />
+            </section>
+        </main>
+    )
+}
